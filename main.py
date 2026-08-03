b14ed3e93fbc9cf9b00bdf4674117fde2be6da14
SangkyunLee <lee.sangkyun@gmail.com>
2026-08-03 14:57:47 -0400

    update python code

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt")
-model.export(format="onnx", imgsz=640, opset=12)
 
 
 def main():
-    print("Hello from yolo-test!")
+    model = YOLO("yolov8n.pt")
+    model.export(format="onnx", imgsz=640, opset=12)
+
+    print("yolo model exported!")
 
 
 if __name__ == "__main__":
